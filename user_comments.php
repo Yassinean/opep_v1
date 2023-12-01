a9a66b4bcd58bdd758c5c01ebdb37aeef866f361
Yassine Hanach <yassinedubraska@gmail.com>
2023-12-01 01:16:43 +0100

    last push

--- user_comments.php
+++ user_comments.php
@@ -28,7 +28,7 @@ if (isset($_SESSION['userId'])) {
             <head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>User Cart</title>
+                <title>Votre Panier</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
             </head>
 
